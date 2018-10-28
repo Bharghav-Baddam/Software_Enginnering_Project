882be63a72e3fd9b9295b69e7b8fbe338d2912f8
johnan125 <30758999+johnan125@users.noreply.github.com>
2018-10-28 17:04:38 -0400

    added search page

--- src/main/java/com/buddyfinder/main/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.buddyfinder.main.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.buddyfinder.main.models.Account;
-
-public interface AccountRepository extends JpaRepository<Account, Integer>{
-
-}
--- src/main/java/com/buddyfinder/main/repository/ActivityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.buddyfinder.main.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.buddyfinder.main.models.Activity;
-
-public interface ActivityRepository extends CrudRepository<Activity, String>{
-
-}

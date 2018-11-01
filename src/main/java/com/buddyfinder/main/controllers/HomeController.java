71c6476746fdd781878e249dd1f7b4dd9d43170d
cwnc2013 <43189220+cwnc2013@users.noreply.github.com>
2018-11-01 14:19:17 -0400

    Update src/main/java/com/buddyfinder/main/controllers/HomeController.java

--- src/main/java/com/buddyfinder/main/controllers/HomeController.java
+++ src/main/java/com/buddyfinder/main/controllers/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.buddyfinder.main.models.Account;
 import com.buddyfinder.main.repository.AccountRepository;
 import com.buddyfinder.main.services.AuthService;
 
